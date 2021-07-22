f10c1ef17e0a45c365b86a226f370037be2340bb
Lucy Awrey <35198881+lucyawrey@users.noreply.github.com>
2021-07-22 09:31:34 -0400

    Updated personal site link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import styles from "../styles/Footer.module.scss";
 function Footer() {
   return (
       <footer className={styles.footer}>
-        <a href="https://lucyawrey.com" target="_blank" title="Created by Lucy Awrey">
+        <a href="https://lucyawrey.com" target="_blank" rel="noreferrer" title="Created by Lucy Awrey">
           Created by Lucy Awrey
         </a>
       </footer>
